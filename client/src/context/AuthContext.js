b2a73ca6e7bc64d2e496946726500697d15a0eaa
anjithavk823 <anjithavk823@gmail.com>
2026-02-05 12:19:37 +0300

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -6,16 +6,33 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(localStorage.getItem("token"));
 
+  // Run when app loads
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    const savedUser = localStorage.getItem("user");
+
+    if (savedToken && savedUser) {
+      setToken(savedToken);
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
+
+  // Login function
   const login = (userData, jwtToken) => {
     setUser(userData);
     setToken(jwtToken);
+
     localStorage.setItem("token", jwtToken);
+    localStorage.setItem("user", JSON.stringify(userData));
   };
 
+  // Logout function
   const logout = () => {
     setUser(null);
     setToken(null);
+
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
   };
 
   return (
