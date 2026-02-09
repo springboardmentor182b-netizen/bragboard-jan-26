eb6f1e6aeeade220aa6514c7ddbe49237cb543b3
yazn42 <yasinnisay005@gmail.com>
2026-02-09 20:58:42 +0530

    Add Admin Report Moderation client UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminReportModeration from "./pages/AdminReportModeration";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin/reports" element={<AdminReportModeration />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
