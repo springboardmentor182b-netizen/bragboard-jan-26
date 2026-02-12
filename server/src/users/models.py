35e25b8cab337939ff0913bb38aa9cad34675b4a
anjithavk823 <anjithavk823@gmail.com>
2026-02-12 15:54:35 +0300

    Update models.py

---
+++ server/src/users/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Enum, TIMESTAMP
+from sqlalchemy.sql import func
+from src.core.database import Base
+import enum
+
+
+class RoleEnum(str, enum.Enum):
+    employee = "employee"
+    admin = "admin"
+
+
+class User(Base):
+    tablename = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    department = Column(String, nullable=False)
+    role = Column(Enum(RoleEnum), default=RoleEnum.employee)
+    joined_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
