ee7c74a7e1967e7f54a362daf891c9fce84646e2
anjithavk823 <anjithavk823@gmail.com>
2026-02-12 16:07:18 +0300

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from src.api import api_router
-from src.database.core import Base, engine
+from src.core.database import Base, engine
+from src.users.api import router as user_router
+
+app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="BragBoard Backend")
-
-app.include_router(api_router)
+app.include_router(user_router)
