80889be32ec02a7739cd231230026d80909f38ad
Liyanaftva <118064000+Liyanaftva@users.noreply.github.com>
2026-04-16 16:37:24 +0530

    Update AuthContext.js
    Fix AuthContext.js syntax errors

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -36,5 +36,9 @@ export const AuthProvider = ({ children }) => {
 };
 
 export const useAuth = () => {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 };
